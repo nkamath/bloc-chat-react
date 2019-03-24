32d2370f23f52ed96943501aafadf2d6e86bc0b4
Nritya Kamath <nritya.k@gmail.com>
2019-03-24 16:19:44 -0700

    Message entry box added -- functionality incomplete

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -4,10 +4,34 @@ import User from "./chatbox/User.js";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+
 
 
 
 class ChatBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messageContent: "",
+    };
+
+    this.messagesRef = this.props.firebase.database().ref('messages');
+  };
+
+  handleSendMessage(){
+    console.log("send message: " + this.state.messageContent);
+  }
+
+  handleMessageContentChange(event){
+    this.setState({
+      messageContent: event.target.value
+    });
+  }
+
   render() {
     return(
       <Container>
@@ -28,6 +52,22 @@ class ChatBox extends Component {
               firebase={this.props.firebase}
               activeRoom={this.props.activeRoom}
               />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <InputGroup className="mb-3">
+            <FormControl
+              placeholder="Message..."
+              aria-label="Message..."
+              aria-describedby="basic-addon2"
+              value={this.state.messageContent}
+              onChange={(e) => this.handleMessageContentChange(e)}
+            />
+            <InputGroup.Append>
+              <Button disabled = {false} onClick = {() => this.handleSendMessage()} variant="outline-primary">Send</Button>
+            </InputGroup.Append>
+          </InputGroup>
           </Col>
         </Row>
       </Container>
